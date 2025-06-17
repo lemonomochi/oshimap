6f08693dc5698648d7c944820a895cbfc149563e
lemonomochi <nana94js@gmail.com>
2025-06-17 21:37:49 +0900

    add pin

--- script.js
+++ script.js
@@ -13,32 +13,14 @@ L.tileLayer(`https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=${apiKey}`
 
 // アイコンをタイプごとに用意
 const iconMap = {
-  drama: L.icon({ iconUrl: './img/drama_icon.png', iconSize: [30, 43], iconAnchor: [15, 40] }),
-  eat:  L.icon({ iconUrl: './img/eat_icon.png',  iconSize: [30, 43], iconAnchor: [15, 45] }),
-  vlog: L.icon({ iconUrl: './img/vlog_icon.png', iconSize: [30, 43], iconAnchor: [15, 40] })
-//   default: L.icon({ iconUrl: 'icons/default.png', iconSize: [30, 40], iconAnchor: [15, 40] })
+  drama: L.icon({ iconUrl: '/img/drama_icon.png', iconSize: [50, 66], iconAnchor: [25, 66] }),
+  eat:  L.icon({ iconUrl: '/img/eat_icon.png',  iconSize: [50, 66], iconAnchor: [25, 66] }),
+  vlog: L.icon({ iconUrl: '/img/vlog_icon.png', iconSize: [50, 66], iconAnchor: [25, 66] }),
+  venue: L.icon({ iconUrl: '/img/venue_icon.png', iconSize: [50, 66], iconAnchor: [25, 66] }),
+  default: L.icon({ iconUrl: '/img/drama_icon.png', iconSize: [50, 66], iconAnchor: [25, 66] })
 };
 
 
-// CSVを読み込んでマーカーを追加
-// Papa.parse('places.csv', {
-//   download: true,
-//   header: true,
-//   complete: function(results) {
-//     results.data.forEach(place => {
-//       const lat = parseFloat(place.lat);
-//       const lng = parseFloat(place.lng);
-//       const type = place.movieType;
-
-//       const icon = iconMap[type] || iconMap.default;
-
-//       L.marker([lat, lng], { icon: icon }).addTo(map)
-//         .on('click', function() {
-//           showPlaceDetails(place); // ここはピン押したときの処理
-//         });
-//     });
-//   }
-// });
 
 Papa.parse("csv/places.csv", {
   download: true,
@@ -46,7 +28,7 @@ Papa.parse("csv/places.csv", {
   complete: function(results) {
     results.data.forEach(place => {
       const type = place.move_type;
-      const icon = iconMap[type];
+      const icon = iconMap[type] || iconMap.default;
 
       if (place.lat && place.lng) {
         const marker = L.marker([parseFloat(place.lat), parseFloat(place.lng)], { icon: icon }).addTo(map);
